d4f9a3f2e8f7bd380d67804784dbcd5fe8ea30a3
Afonso Sousa <afonso.sousa@sap.com>
2021-01-03 12:34:05 +0000

    Changes for the app to work

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras import backend as k
 from werkzeug.utils import secure_filename
 from wtforms import SubmitField
-
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -21,8 +21,31 @@ photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app)  # set maximum file size, default is 16MB
 
+
+# custom metrics
+def recall_m(y_true, y_pred):
+    true_positives = k.sum(k.round(k.clip(y_true * y_pred, 0, 1)))
+    possible_positives = k.sum(k.round(k.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + k.epsilon())
+    return recall
+
+
+def precision_m(y_true, y_pred):
+    true_positives = k.sum(k.round(k.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = k.sum(k.round(k.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + k.epsilon())
+    return precision
+
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall + k.epsilon()))
+
+
 # Load the model:
-cnn_model = load_model('cats_and_dogs_model9.h5')
+cnn_model = load_model('NoEmpty_LowRes_OverSampling_DataAugmentation.h5',
+                       custom_objects={"f1_m": f1_m, "precision_m": precision_m, "recall_m": recall_m})
 CLASS_INDICES = {0: 'cat', 1: 'dog'}
 
 
@@ -50,14 +73,16 @@ def prediction():
 
 def return_prediction(filename):
     input_image_matrix = _image_process(filename)
+    # img = image.load_img('uploads/' + filename, target_size=(64, 64))
     score = cnn_model.predict(input_image_matrix)
     class_index = cnn_model.predict_classes(input_image_matrix, batch_size=1)
-    
-    return CLASS_INDICES[class_index[0]], score
+
+    # return CLASS_INDICES[class_index[0]], score
+    return class_index[0], score
 
 
 def _image_process(filename):
-    img = image.load_img('uploads/' + filename, target_size=(64, 64))
+    img = image.load_img('uploads/' + filename, target_size=(96,128))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     input_matrix = np.vstack([x])
